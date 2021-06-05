0d82568e92fdc3eda940811d1695069573ee486c
David Wallace <dwallacemarketing@gmail.com>
2021-06-05 12:09:24 -0500

    strings and variables

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,22 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'David'
+age = 1
 
-# String Formatting
+# simple concat
+print(name + ' is only ' + str(age))
 
-# String Methods
+age = 10
+# arguments by position
+print('{name} is only {age}'.format(name=name, age=age))
+
+# f strings in py 3.6+ -similar to a js `template literal - his dog is {age}`
+print(f'my name is {name} and I am {age}')
+
+s = 'this is long boring string for testing different methods'
+print(s.capitalize())
+
+# list of string methods here -> https://www.w3schools.com/python/python_ref_string.asp
+# examples
+print(s.split())
+print(s.isalpha())
--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -1,15 +1,33 @@
 # A variable is a container for a value, which can be of various types
-
 '''
 This is a 
 multiline comment
 or docstring (used to define a functions purpose)
 can be single or double quotes
-'''
 
-"""
 VARIABLE RULES:
   - Variable names are case sensitive (name and NAME are different variables)
   - Must start with a letter or an underscore
-  - Can have numbers but can not start with one
-"""
+  - Can have numbers but can not start with one,
+  - no semi colon needed
+'''
+x = 10  # int
+y = 2.5  # floating number
+z = 'Dave'  # string
+is_active = True  # boolean - T or F must be capitalized or it will be a variable instead of  boolean value
+print(x, y, z, is_active)
+
+# assign multiple values
+x, y, z, is_active = (20, 2.1, 'Ron', False)
+print(x, y, z, is_active)
+
+# simple matth
+a = x + y
+print(a)
+# type of test
+print(type(x))
+# casting - turn value into a string
+x = (str(x))
+print
+# type of test
+print(type(x))
