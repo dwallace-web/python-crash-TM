853c2664837d157ba1b0e18d9b5dd39c110ef7b9
David Wallace <dwallacemarketing@gmail.com>
2021-06-09 20:49:19 -0500

    tuple notes

--- python_crashcoursefiles/tuples_sets.py
+++ python_crashcoursefiles/tuples_sets.py
@@ -1,5 +1,16 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+
+# make a tuple
+cars = ('Rav-4', 'CRV', 'Hummer', 'Mazda big', 'subaru big')
+carz = ('Rav-4')
+carz1 = ('Rav-4',)
+print(cars, type(cars))
+print(carz, type(carz))
+print(carz1, type(carz1))
+
+# delete a tuple
+del carz1
+# print(carz1) -- would return not defined
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
